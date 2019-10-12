8c68d5e0958252d741b8d568fa9c86a5296978da
Fisayomie <44126331+Fisayomie@users.noreply.github.com>
2019-10-12 17:35:23 +0100

    Update test_task10.py

--- test_task10.py
+++ test_task10.py
@@ -1,30 +1,30 @@
+import sys
 import pickle as p
 # import traceback
 from flask import Flask, request, jsonify
 import json
 app = Flask(__name__)
 
-#@app.before_request
-#def exe():
-#    summarizer = 'modelfile.pkl'
-#    model = p.load(open(summarizer, 'rb'))
-
-
 @app.route('/api/summarize', methods=['POST', 'GET'])
 def get_url():
-    """
-    
-    :returns: jsonified-> blah
-    :rtype: string
-    
-    """
-    if request.method == 'POST':
-        url = request.json['theUrl']
-        #print(content)
-        jsonified = jsonify(url), 200
-        return jsonified
+    try:
+        if request.method == 'POST':
+            url = request.json['theUrl']
+            #print(content)
+            return jsonify(url), 200
+    except KeyboardInterrupt:
+    print('You cancelled the operation.')
+    except NameError:
+    print('Not Defined')
+    except:
+        print("An Error occurred") 
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
-
+try:
+    if __name__ == '__main__':
+        app.run(debug=True, host='127.0.0.1', port=5000)
+except KeyboardInterrupt:
+    print('You cancelled the operation.')
+except NameError:
+    print('Not Defined')
+except:
+        print("An exception occurred")
